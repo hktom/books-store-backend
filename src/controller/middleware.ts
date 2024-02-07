5f5fe24c01502c9c2aeb7818aa434109787bfa77
tom hikari <tomprescott1565@gmail.com>
2024-02-07 18:16:00 +0100

    fix api middleware

--- src/controller/middleware.ts
+++ src/controller/middleware.ts
@@ -22,18 +22,21 @@ export class Middleware implements IMiddleware {
       const book = await this.shoppingService.getShoppingItemById(
         req.body.bookId
       );
-      if (!book) return res.status(404);
+      if (!book) return false;
       req.body.book = book;
     }
+
+    return true;
   }
 
   private async cartMiddleware(req: Request, res: Response, next: any) {
     if (req.body.cartId) {
       const cart = await this.cartService.getCartById(req.body.cartId);
-      if (!cart) return res.status(404);
+      if (!cart) return false;
       req.body.cart = cart;
       req.body.order = cart.order;
     }
+    return true;
   }
 
   private async orderMiddleware(req: Request, res: Response, next: any) {
@@ -49,37 +52,51 @@ export class Middleware implements IMiddleware {
         total: 0,
         user: user,
       });
-      if (!orderId) return res.status(500);
+      if (!orderId) return false;
       currentOrder = await this.orderService.getOrderById(orderId);
     }
     req.body.currentOrder = currentOrder;
 
     if (req.body.orderId) {
       const order = await this.orderService.getOrderById(req.body.orderId);
-      if (!order) return res.status(404);
+      if (!order) return false;
       req.body.order = order;
     }
+    return true;
   }
 
   private async userMiddleware(req: Request, res: Response, next: any) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
-    if (token == null) return res.sendStatus(401);
+    if (token == null) return false;
 
     const user = await this.authenticationService.me(token);
 
-    if (!user) return res.sendStatus(401);
+    console.log("user", user);
+
+    if (!user) return false;
 
     req.body.user = user;
     req.body.token = token;
+
+    return true;
   }
 
   async check(req: Request, res: Response, next: any) {
-    await this.userMiddleware(req, res, next);
-    await this.bookMiddleware(req, res, next);
-    await this.orderMiddleware(req, res, next);
-    await this.cartMiddleware(req, res, next);
+    if (!(await this.userMiddleware(req, res, next))) {
+      return res.status(401).send("Unauthorized");
+    }
+    if (!(await this.bookMiddleware(req, res, next))) {
+      return res.status(404).send("Book not found");
+    }
+    if (!(await this.orderMiddleware(req, res, next))) {
+      return res.status(500).send("Internal Server Error");
+    }
+    if (!(await this.cartMiddleware(req, res, next))) {
+      return res.status(500).send("Internal Server Error");
+    }
+
     next();
   }
 }
